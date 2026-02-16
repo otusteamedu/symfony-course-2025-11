664fd061479ee6e4243277aebe5928a1d5a29a40
Sergey Petrov <cl@coders-lair.com>
2026-02-16 15:04:56 +0300

    lesson-18_Queues_Advanced_Practices

--- src/Infrastructure/Repository/TweetRepositoryCacheDecorator.php
+++ src/Infrastructure/Repository/TweetRepositoryCacheDecorator.php
@@ -47,6 +47,7 @@ readonly class TweetRepositoryCacheDecorator implements TweetRepositoryInterface
                     static fn (Tweet $tweet): TweetModel => new TweetModel(
                         $tweet->getId(),
                         $tweet->getAuthor()->getLogin(),
+                        $tweet->getAuthor()->getId(),
                         $tweet->getText(),
                         $tweet->getCreatedAt(),
                     ),
